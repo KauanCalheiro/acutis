18a9ea1e359229341ef9ca6be615c1b78291a13a
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2026-07-05 18:43:29 -0300

    refactor: expose backend clients in a nitro util

--- frontend/server/api/projects/clone.post.ts
+++ frontend/server/api/projects/clone.post.ts
@@ -1,20 +1,8 @@
-import type { FetchError } from 'ofetch'
-
 export default defineEventHandler(async (event) => {
-  const { apiUrl } = useRuntimeConfig(event)
+  const { acutis } = useClients(event)
 
-  try {
-    return await $fetch('/api/v1/projects/create/clone', {
-      baseURL: apiUrl,
-      method: 'POST',
-      body: await readBody(event)
-    })
-  } catch (error) {
-    const err = error as FetchError
-
-    throw createError({
-      statusCode: err.statusCode ?? 500,
-      data: err.data
-    })
-  }
+  return acutis('/api/v1/projects/create/clone', {
+    method: 'POST',
+    body: await readBody(event)
+  })
 })
