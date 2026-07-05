1a5ea4ad92e77db033e0972bfaa3b0defe5cf13b
Kauan Calheiro <kauan.calheiro@universo.univates.br>
2026-07-05 19:24:27 -0300

    style: use primary lg badge for the project origin

--- frontend/app/components/project/card.vue
+++ frontend/app/components/project/card.vue
@@ -43,8 +43,8 @@ const origin = computed(() => {
         {{ project.repository ?? project.path }}
       </p>
       <UBadge
-        color="neutral"
         variant="soft"
+        size="lg"
         :icon="origin.icon"
         :label="origin.label"
         class="self-start"
