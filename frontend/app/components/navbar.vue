7fd6f9f3643ef57c5febeeb4b575ec388374f43c
Kauan Calheiro <110562396+KauanCalheiro@users.noreply.github.com>
2026-07-05 21:23:43 -0300

    style: expand navbar from anywhere except the bottom controls (#48)

--- frontend/app/components/navbar.vue
+++ frontend/app/components/navbar.vue
@@ -55,14 +55,12 @@ const items = [
 
 <template>
   <aside
-    class="sticky top-6 my-6 lg:top-10 lg:my-10 mr-3 flex h-[calc(100vh-3rem)] lg:h-[calc(100vh-5rem)] shrink-0 flex-col justify-between overflow-hidden rounded-r-xl bg-accented/50 p-3 shadow-[6px_0_10px_-6px_rgb(0_0_0/0.12)] dark:shadow-[6px_0_10px_-6px_rgb(0_0_0/0.55)] transition-[width] duration-300"
+    class="sticky top-6 my-6 lg:top-10 lg:my-10 mr-3 flex h-[calc(100vh-3rem)] lg:h-[calc(100vh-5rem)] shrink-0 flex-col justify-between overflow-hidden rounded-r-xl bg-accented/50 p-3 shadow-[6px_0_14px_-6px_rgb(0_0_0/0.28)] dark:shadow-[6px_0_14px_-6px_rgb(0_0_0/0.8)] transition-[width] duration-300"
     :class="expanded ? 'w-56' : 'w-18'"
+    @mouseover="hovering = true"
     @mouseleave="hovering = false"
   >
-    <div
-      class="flex flex-col gap-6"
-      @mouseenter="hovering = true"
-    >
+    <div class="flex flex-col gap-6">
       <NuxtLink
         to="/"
         class="flex items-center gap-3 overflow-hidden"
@@ -109,7 +107,10 @@ const items = [
       </nav>
     </div>
 
-    <div class="flex flex-col gap-2">
+    <div
+      class="flex flex-col gap-2"
+      @mouseover.stop
+    >
       <UPopover
         v-model:open="colorPickerOpen"
         :content="{
